55c863f9c2013198fd6ee5b345c4b1c7bb2df906
Anna <159029258+Malvochka@users.noreply.github.com>
2024-02-17 21:07:37 +0300

    Update equation.cpp

--- equation/equation.cpp
+++ equation/equation.cpp
@@ -1,1 +1,11 @@
-﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double a, b, c;
+	setlocale(LC_ALL, "Russian");
+	cout << "Решение уравнения ax^2 + bx + c = 0\n";
+
+}﻿
