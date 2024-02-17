ad1ff7930a8be59ffe4d6d47579f2de0d0d64e66
Anna <159029258+Malvochka@users.noreply.github.com>
2024-02-17 21:11:22 +0300

    Update equation.cpp

--- equation/equation.cpp
+++ equation/equation.cpp
@@ -2,10 +2,20 @@
 #include <cmath>
 using namespace std;
 
+double disc(double a, double b, double c)
+{
+	return b*b-4.0*a*c;
+}
+
 int main()
 {
 	double a, b, c;
 	setlocale(LC_ALL, "Russian");
 	cout << "Решение уравнения ax^2 + bx + c = 0\n";
+        cout<<" Введите коэффициент a: \n"; cin>>a;
+        cout<<" Введите коэффициент b: \n"; cin>>b;
+	cout<<" Введите коэффициент c: \n"; cin>>c;
+	double d= disc(a,b,c);
+
 
 }﻿
