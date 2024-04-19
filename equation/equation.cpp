f2f8931d263286c8604b465f37b1bb79dabb3b00
Anna <159029258+Malvochka@users.noreply.github.com>
2024-04-20 01:17:09 +0300

    Update equation.cpp

--- equation/equation.cpp
+++ equation/equation.cpp
@@ -2,31 +2,45 @@
 #include <cmath>
 using namespace std;
 
+//Чтение коэффициентов
+void readcoeff(double&a,double&b,double&c)
+{
+	cout<<"\nВведите коэффициент a: "; cin>>a;
+	if(a==0.0)
+	{
+		cout<<"Коэффициент а не может быть равным 0. Уравнение не является квадратным";
+		return 1;
+	}
+	 cout<<"\nВведите коэффициент b: "; cin>>b;
+	cout<<"\nВведите коэффициент c: "; cin>>c;
+	
+}
+//дискриминант
 double disc(double a, double b, double c)
 {
 	return b*b-4.0*a*c;
 }
 
-void answ(double a, double b, double d)
+//печать
+void printsol(double a,double b, double d)
 {
-	double x1,x2;
-	if(d<0.0)
+         if(d<0.0)
 	{
 		cout<<"\nДействительных корней нет";
 	}
 	else 
 		if (d==0)
 		{
-			x1=x2=-b/(2*a);
-			cout<<"\nУравнение имеет единственный корень\n"<<" x = "<<x1;
+			double x=-b/(2*a);
+			cout<<"\nУравнение имеет единственный корень\n"<<" x = "<<x;
 		}
 	        else 
 		{
-			x1=(-b-sqrt(d))/(2*a);
-			x2=(-b+sqrt(d))/(2*a);
+			double x1=(-b-sqrt(d))/(2*a);
+			double x2=(-b+sqrt(d))/(2*a);
 			cout<<"\nУравнение имеет два корня:\n"<<"x1 = " <<x1<<"\nx2 = "<<x2;
 
-		}
+		}	
 }
 
 int main()
@@ -34,15 +48,8 @@ int main()
 	double a, b, c;
 	setlocale(LC_ALL, "Russian");
 	cout << "Решение уравнения ax^2 + bx + c = 0\n";
-        cout<<"\nВведите коэффициент a: "; cin>>a;
-	if(a==0.0)
-	{
-		cout<<"Коэффициент а не может быть равным 0. Уравнение не является квадратным";
-		return 1;
-	}
-        cout<<"\nВведите коэффициент b: "; cin>>b;
-	cout<<"\nВведите коэффициент c: "; cin>>c;
+	readcoeff(a,b,c);
 	double d= disc(a,b,c);
-        answ(a,b,d);
+        printsol(a,b,d);
 	return 0;
 }
